465f5d071e8bd78ab8dbee1e9d52b6024e1d0a80
Eltrac <hi@guhub.cn>
2024-12-07 11:18:47 +0800

    feat(UI): add indicators for planets and signs

--- src/board.js
+++ src/board.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GridHelper from "./utils/GridHelper";
+import { signNames, planetNames } from "./data/elements";
 
 export default function WarstroBoard({ ctx, G, moves }) {
   const gridHelper = new GridHelper(G.grid)
@@ -7,19 +8,36 @@ export default function WarstroBoard({ ctx, G, moves }) {
 
   return (
     <div id="warstro-board">
-      {rows.map((row, index) =>
-        <div
-          className="warstro-row"
-          key={`row-${index}`}
-        >
-          {row.map(cell =>
-            <div
-              className="warstro-cell"
-              key={`${cell.x}-${cell.y}`}
-            >{cell.data.playerID}</div>
-          )}
-        </div>
-      )}
+
+      <div class="warstro-indicators"
+        id="warstro-planet-indicators">{
+        planetNames.map(planet =>{
+          if (planet!=='sun' && planet!=='moon') return <div class="warstro-indicator">{planet}</div>
+          else return null
+        })
+      }</div>
+      <div class="warstro-indicators"
+        id="warstro-sign-indicators">{
+        signNames.map(sign =>
+          <div class="warstro-indicator">{sign}</div>
+        )
+      }</div>
+
+      <div id="warstro-cells">
+        {rows.map((row, index) =>
+          <div
+            className="warstro-row"
+            key={`row-${index}`}
+          >
+            {row.map(cell =>
+              <div
+                className="warstro-cell"
+                key={`${cell.x}-${cell.y}`}
+              >{cell.data.playerID}</div>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
