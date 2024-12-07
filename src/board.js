ccb5db81899b1ec2c86489b01a63919a0f9ea115
Eltrac <hi@guhub.cn>
2024-12-07 11:49:07 +0800

    fix: unique key for each indicator

--- src/board.js
+++ src/board.js
@@ -10,11 +10,19 @@ export default function WarstroBoard({ ctx, G, moves }) {
 
       <div className="warstro-indicators"
         id="warstro-planet-indicators">{
-          G.planetSequence.map(item => <div className="warstro-indicator">{item}</div>)
+          G.planetSequence.map((item, index) =>
+            <div className="warstro-indicator"
+              key={index}
+            >{item}</div>
+          )
       }</div>
       <div className="warstro-indicators"
         id="warstro-sign-indicators">{
-          G.signSequence.map(item => <div className="warstro-indicator">{item}</div>)
+          G.signSequence.map((item, index) =>
+            <div className="warstro-indicator"
+              key={index}
+            >{item}</div>
+          )
       }</div>
 
       <div id="warstro-cells">
