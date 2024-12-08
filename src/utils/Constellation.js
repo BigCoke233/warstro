fd2a4646906120708ae8e20deaaf9d614b868af9
Eltrac <hi@guhub.cn>
2024-12-08 10:33:43 +0800

    feat(game-logic): sunshone and moonlit planets affecting descending shape

--- src/utils/Constellation.js
+++ src/utils/Constellation.js
@@ -9,16 +9,27 @@ class Constellation {
   constructor(G, sign, planet) {
     this.planetSequence = G.planetSequence
     this.signSequence = G.signSequence
+
     this.sign = signs[this.signSequence[sign-1]]
     this.planet = planets[this.planetSequence[planet-1]]
+
+    // if sun or moon exists on this row
+    this.sunshone = G.movingCelestials.sun === planet
+    this.moonlit = G.movingCelestials.moon === sign
   }
 
   hasSameElement() {
-    return this.planet.element===this.sign.element
+    let withLocalPlanet = this.planet.element===this.sign.element
+    let withSun = this.sunshone ? this.sign.element===planets.sun.element : false
+    let withMoon = this.moonlit ? this.sign.element===planets.moon.element : false
+    return withLocalPlanet || withSun || withMoon
   }
 
   isRuled() {
-    return this.planet.ruling.includes(this.sign)
+    let withLocalPlaent = this.planet.ruling.includes(this.sign)
+    let withSun = this.sunshone ? planets.sun.ruling.includes(this.sign) : false
+    let withMoon = this.moonlit ? planets.moon.ruling.includes(this.sign) : false
+    return withLocalPlaent || withSun || withMoon
   }
 }
 
