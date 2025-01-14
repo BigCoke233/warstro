67d38be0d344ee91b5285025008b16cb93a38df0
Eltrac <hi@guhub.cn>
2025-01-14 10:46:22 +0800

    remove: event text indicator in action display

--- src/ui/ActionDisplay.js
+++ src/ui/ActionDisplay.js
@@ -9,11 +9,6 @@ export default function ActionDisplay(props) {
         <Dice data={{ G: G, type: "sign", index: x }} />
         <Dice data={{ G: G, type: "plaent", index: y }} />
       </div>
-      {G.last.picked.by!==null && (
-        <div id="warstro-cardpick">
-          <p>player {G.last.picked.by} picked {G.last.picked.card.type}({G.last.picked.card.name})</p>
-        </div>
-      )}
     </div>
   )
 }
